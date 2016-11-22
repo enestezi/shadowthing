02c10ab6f2b9960102c34352983758d6ad2120ca
enestezi <enestezi@hotmail.com>
2016-11-22 14:13:05 +0100

    rotate is alive

--- New Unity Project/New Unity Project/Assets/Scripts/mausradDreh.cs
+++ New Unity Project/New Unity Project/Assets/Scripts/mausradDreh.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class mausradDreh : MonoBehaviour {
+public class mausradDreh : MonoBehaviour
+{
+	public float mausradDrehung;        //stores the rotation of the mouse wheel
+	private float geschwindigkeit = 1500.0f;      //multiplier for the mouse wheel input
+	private Vector3 objPos;
 
-	private GameObject interaktiv1;
+	private GameObject interaktiv1Dreh;
+	private Rigidbody2D interaktiv1Rigid;
 
-	void Start () {
-		interaktiv1 = GameObject.FindWithTag ("interakiv1");
-
-		if (interaktiv1.GetComponent<HingeJoint>()) {
-			interaktiv1 = interaktiv1.AddComponent<HingeJoint2D> () as HingeJoint2D;
-			GetComponent<HingeJoint>().connectedBody = interaktiv1.GetComponent<Rigidbody2D>();
-
-		}
+	void Start ()
+	{
+		interaktiv1Dreh = GameObject.FindWithTag ("interakiv1");
+		interaktiv1Rigid = interaktiv1Dreh.GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+		mausradDrehung = Input.GetAxis("Mouse ScrollWheel") * geschwindigkeit * Time.deltaTime;
+
+		if (mausradDrehung != 0)
+		{
+			interaktiv1Rigid.freezeRotation = false;
+
+			if (mausradDrehung > 0) {
+				interaktiv1Rigid.AddTorque(mausradDrehung * geschwindigkeit);
+			}
+			if (mausradDrehung < 0) {
+				interaktiv1Rigid.AddTorque(-(mausradDrehung * geschwindigkeit));
+			}
+		}
 	}
 }
