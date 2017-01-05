b62ef3a0b03ca7373860fe025788cbf8628af8e6
enestezi <enestezi@hotmail.com>
2017-01-05 23:36:31 +0100

    menu etc.

---
+++ New Unity Project/New Unity Project/Assets/Scripts/InteraktivMenuScripts/ScrollRectSnap_Vertikal.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // um ui element zu rufen
+
+public class ScrollRectSnap_Vertikal : MonoBehaviour {
+
+	public RectTransform panel; //to hold the scrollpanel
+	public Button[] bttn;
+	public RectTransform center; //zentrum to vergleich zwischen butttons array
+
+	private float[] distance; //alle buttuns dinstance to zentrum array
+
+	private float[] distReposition;
+
+	private bool dragging = false; //true beim dragging, weil UI beim drag versuchen wird, buttons zu fangen. das muss man verhindern. beim drag wir snap enabled
+	private int bttnDistance; // distance zwischen buttons
+	private int minButtonNum; // buttonzahl mit niedrigest distance to zentrum
+	private int bttnLenght;
+
+	void Start()
+	{
+		bttnLenght = bttn.Length;
+		distance = new float[bttnLenght];
+
+		distReposition = new float[bttnLenght];
+
+		//button distance
+		bttnDistance = (int)Mathf.Abs(bttn[1].GetComponent<RectTransform>().anchoredPosition.y - bttn[0].GetComponent<RectTransform>().anchoredPosition.y); //buttonone= 300 - butten2=0 == 300 buttondist
+		print (bttnDistance);
+	}
+
+}
---
+++ New Unity Project/New Unity Project/Assets/Scripts/InteraktivScripts/TastaturController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Script für Tastaturkontroll
+public class TastaturController : MonoBehaviour {
+
+	private Rigidbody2D rb_Lager;	 		// Lager für Rigidbody2D, das von ObjektGeklickt script kommt 	
+	public float geschwindigkeit = 20;      // Bewegungsgeschwindigkeit    
+
+	private float horizontal;
+	private float vertical;
+	private Vector2 bewegung;
+
+	void FixedUpdate()
+	{
+		horizontal = Input.GetAxis ("Horizontal");
+		vertical = Input.GetAxis ("Vertical");
+
+		bewegung = new Vector2 (horizontal * geschwindigkeit, vertical * geschwindigkeit);
+
+		if (rb_Lager != null) //wenn rigidbody2d geholt ist
+		{
+			rb_Lager.velocity = bewegung;	//bewege
+		}
+	}
+
+	// Funktion zum entsprechendes Rigidbody2D zu holen
+	public void Rb_holen (Rigidbody2D rb_geklickt)
+	{
+		rb_Lager = rb_geklickt;
+
+	}
+}
